f97b8df46e5afb14230d5e5d7b8749a531afaa7b
datny <robert.przydatek.cl@gmail.com>
2019-06-04 10:14:20 +0200

    dynamic progr approach- w/o items list

--- README.txt
+++ README.txt
@@ -1,5 +1,1 @@
-Provided program picks memes from list, sorts them by most atrractive size/cost ratio
-and returns tuple that contains total value of choosen memes, and set of their names.
-
-If any file provided in list is corupted, its skipped. Same with negative size or cost.
-
+Used dynamic programming(I/O) to solve problem in proper and memory efficient way.
--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-def sort_by_single_mb_cost(memes_list):
-    """Sorts original memes list, by new number
-    that is created by dividing price/size"""
-    memes_list.sort(key=lambda meme: meme[2] / meme[1], reverse=True)
-
-
 def calculate(usb_size, memes):
-    """Calculate optimal set of memes that will
-    fit on pendrive of fixed size. """
-    usb_size_mb = usb_size*1024
-    sort_by_single_mb_cost(memes)
-    set_of_memes = set()
-    total_income = 0
-    usb_used_space = 0
+    W = usb_size * 1024
+    memes = set(memes)
+    n = len(memes)
+    val = []
+    wt = []
+    memes_names = []
     for meme in memes:
-        if (usb_used_space + meme[1] > usb_size_mb) \
-                or ((meme[1] < 0) or (meme[2]) < 0):
-            continue
-        elif meme[0] not in set_of_memes:
-            usb_used_space += meme[1]
-            total_income += meme[2]
-            set_of_memes.add(meme[0])
-    return total_income, set_of_memes
+        val.append(meme[2])
+        wt.append(meme[1])
+        memes_names.append(meme[0])
+
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+
+            elif wt[i - 1] <= w:
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
 
 
+
+    return (K[n][W],"set of memes")
+
+
+usb_size = 1
+memes = [
+('rollsafe.jpg', 205, 6),
+('sad_pepe_compilation.gif', 410, 10),
+('yodeling_kid.avi', 605, 12)
+]
+
+print(calculate(usb_size, memes))
